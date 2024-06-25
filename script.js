53de0df7525fe3fa924d25a365a99e4124c35688
Russellier <russellvap13@gmail.com>
2024-06-25 12:36:32 +0800

    feat: Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 "use strict";
 
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  let computerChoice;
+  switch(randomNumber) {
+    case 0:
+      computerChoice = 'rock';
+      break;
+    case 1:
+      computerChoice =  'paper';
+      break;
+    case 2:
+      computerChoice =  'scissors';
+      break;
+  }
+  return computerChoice;
+}
+
+console.log(getComputerChoice());
